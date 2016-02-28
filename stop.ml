a76f8c388c0f6d8028db50e8768e175239973fa3
James Stenger <jms2431@columbia.edu>
2016-02-28 17:56:41 +0000

    added test; dice folder; legacy folder; updating printing, ast, parser, scanner

--- stop.ml
+++ stop.ml
@@ -1,17 +1,15 @@
-open Parser
-
 type action = Ast | LLVM_IR | Compile
 
 let _ = 
-    (*
     let action = if Array.length Sys.argv > 1 then
-        List.assoc Sys.argv(1) [ ("-a", Ast);       (* Print the AST only *)
+        List.assoc Sys.argv.(1) [ ("-a", Ast);       (* Print the AST only *)
                                  ("-l", LLVM_IR);   (* Generate LLVM, don't check *)
-                                 ("-c", Compile); ] (* Generate, check LLVM IR *)
+                                 ("-c", Compile) ] (* Generate, check LLVM IR *)
     else Ast in
-    *)
-    let action = Ast in 
     let lexbuf = Lexing.from_channel stdin in 
     let ast = Parser.expr Scanner.token lexbuf in
     match action with
         Ast -> print_string (Ast.string_of_program ast)
+        | LLVM_IR -> print_string "Not Yet Implemented"
+        | Compile -> print_string "Not Yet Implemented"
+

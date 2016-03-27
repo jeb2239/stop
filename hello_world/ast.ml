f5d9231f053bbf4662676d3a3b195905a0e6a105
Jonathan Barrios <jeb2239@columbia.edu>
2016-03-27 15:52:14 -0400

    started pretty printer

--- hello_world/ast.ml
+++ hello_world/ast.ml
@@ -33,6 +33,7 @@ type expr =
 
 
 
+
 and stmt =
     Block of stmt list
   | Expr of expr
@@ -43,3 +44,74 @@ and stmt =
 
 
 type program = Program of stmt list
+
+
+
+
+let string_of_op = function
+    Add -> "+"
+  | Sub -> "-"
+  | Mult -> "*"
+  | Div -> "/"
+  | Equal -> "=="
+  | Neq -> "!="
+  | Less -> "<"
+  | Leq -> "<="
+  | Greater -> ">"
+  | Geq -> ">="
+  | And -> "&&"
+  | Or -> "||"
+
+let string_of_uop = function
+    Neg -> "-"
+  | Not -> "!"
+
+
+
+
+let rec string_of_dtype = function
+	Int_t -> "Int"
+	| Float_t-> "Float"
+	| Unit_t -> "Unit"
+	| Bool_t -> "Bool"
+	| Name_t(a) -> a
+	| Functiontype(formlist,dt) -> 
+	| Arraytype(dt,a) -> (string_of_dtype dt) ^ "[" ^ (string_of_int a) ^ "]"
+	and string_of_formal_list = function
+	| Formal(dt,a) -> 
+
+
+let rec string_of_expr = function
+  | IntLit(a) -> string_of_int a
+  | FloatLit(a) -> string_of_float a
+  | FuncLit(fparam,dt,sl) ->
+  | BoolLit(true) -> "true"
+  | BoolLit(false) -> "false"
+  | Id(s) -> s
+  | Binop(e1, o, e2) ->
+      string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
+  | Unop(o, e) -> string_of_uop o ^ string_of_expr e
+  | Assign(v, e) -> v ^ " = " ^ string_of_expr e
+  | Call(f, el) ->
+      f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
+  |
+  | Noexpr -> ""
+
+let rec string_of_stmt = function
+    Block(stmts) ->
+      "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
+  | Expr(expr) -> string_of_expr expr ^ ";\n";
+  | Return(expr) -> "return " ^ string_of_expr expr ^ ";\n";
+  | If(e, s, Block([])) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
+  | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^
+      string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
+  | For(e1, e2, e3, s) ->
+      "for (" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^
+      string_of_expr e3  ^ ") " ^ string_of_stmt s
+  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
+
+let string_of_typ = function
+    Int -> "int"
+  | Bool -> "bool"
+  | Void -> "void"
+
--- hello_world/parser.mly
+++ hello_world/parser.mly
@@ -56,7 +56,7 @@ literals:
     | TRUE              { BoolLit(true) }
     | FALSE             { BoolLit(false) }
     | ID                { Id($1) }
-    | fun_lit 			{ }
+    | fun_lit 			{ $1 }
 
 fun_lit:
 	LPAREN formal_list RPAREN COLON dtype stmts {FuncLit($2,$5,$6)}
@@ -96,3 +96,36 @@ fun_type:
 
 
 
+expr:
+    literals          { $1 }
+  | TRUE             { BoolLit(true) }
+  | FALSE            { BoolLit(false) }
+  | ID               { Id($1) }
+  | expr PLUS   expr { Binop($1, Add,   $3) }
+  | expr MINUS  expr { Binop($1, Sub,   $3) }
+  | expr TIMES  expr { Binop($1, Mult,  $3) }
+  | expr DIVIDE expr { Binop($1, Div,   $3) }
+  | expr EQ     expr { Binop($1, Equal, $3) }
+  | expr NEQ    expr { Binop($1, Neq,   $3) }
+  | expr LT     expr { Binop($1, Less,  $3) }
+  | expr LEQ    expr { Binop($1, Leq,   $3) }
+  | expr GT     expr { Binop($1, Greater, $3) }
+  | expr GEQ    expr { Binop($1, Geq,   $3) }
+  | expr AND    expr { Binop($1, And,   $3) }
+  | expr OR     expr { Binop($1, Or,    $3) }
+  | MINUS expr %prec NEG { Unop(Neg, $2) }
+  | NOT expr         { Unop(Not, $2) }
+  | ID ASSIGN expr   { Assign($1, $3) }
+  | ID LPAREN actuals_opt RPAREN { Call($1, $3) } /*if we call a named function*/
+  | fun_lit            {$1} 
+  | LPAREN expr RPAREN { $2 }
+
+
+
+actuals_opt:
+    /* nothing */ { [] }
+  | actuals_list  { List.rev $1 }
+
+actuals_list:
+    expr                    { [$1] }
+  | actuals_list COMMA expr { $3 :: $1 }

0b140867ab525c8df3ab2672d6ebdc3d64341292
James Stenger <jms2431@columbia.edu>
2016-05-03 00:45:49 +0000

    attempted fix

---
+++ src/exceptions.ml
@@ -0,0 +1,49 @@
+(* Stop Exceptoins *)
+exception InvalidOption of string 
+exception InvalidArgc
+exception NoFileArgument
+
+(* Scanner Exceptions *)
+exception IllegalCharacter of string * string * int
+
+(* Parser Exceptions *)
+exception CannotDefineVariableLengthArgFunction
+
+(* Generator Exceptions *)
+exception MissingEOF
+
+(* Semant Exceptions *)
+exception ThisUsedOutsideClass
+exception MissingMainFunction
+exception MultipleMainFunctions
+exception InvalidUnaryOperation
+exception UnexpectedNoexpr
+exception InvalidBinaryOperation
+exception UndefinedId of string
+exception DuplicateField of string
+exception DuplicateClassName of string
+exception DuplicateLocal of string
+exception DuplicateFunctionName of string
+exception FunctionNameReserved of string
+exception ReturnTypeMismatch of string * string * string option
+exception AssignmentTypeMismatch of string * string
+exception LocalAssignmentTypeMismatch of string * string
+exception ArrayAccess of string
+exception UndefinedFunction of string
+
+(* Utils Exceptions *)
+exception UtilsError of string
+
+(* Codegen Exceptions *)
+exception PrintfFirstArgNotString
+exception PrintfMissingArgs
+exception NotImplemented
+exception FloatOpNotSupported
+exception IntOpNotSupported
+exception InvalidBinopEvaluationType
+exception InvalidStructType of string
+exception InvalidStructType of string
+exception InvalidDatatype of string
+exception LLVMFunctionNotFound of string
+exception FunctionWithoutBasicBlock of string
+exception AssignmentLhsMustBeAssignable
